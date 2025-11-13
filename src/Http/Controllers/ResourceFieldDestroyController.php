7db22fccb92e071d0c1809c2babc3c3b4509e766
Matthew <matthewjumpsoffbuildings@gmail.com>
2025-11-13 20:51:07 +1030

    Enhance field retrieval in ResourceFieldDestroyController
    Refactor field retrieval logic to handle Flexible fields.

--- src/Http/Controllers/ResourceFieldDestroyController.php
+++ src/Http/Controllers/ResourceFieldDestroyController.php
@@ -6,6 +6,7 @@ use Illuminate\Routing\Controller;
 use Laravel\Nova\DeleteField;
 use Laravel\Nova\Fields\File;
 use Laravel\Nova\Http\Requests\UpdateResourceRequest;
+use Whitecube\NovaFlexibleContent\Flexible;
 
 abstract class ResourceFieldDestroyController extends Controller
 {
@@ -30,7 +31,34 @@ abstract class ResourceFieldDestroyController extends Controller
         $resource->authorizeToUpdate($request);
 
         $fields = $resource->updateFields($request);
-        $field = $fields->findFieldByAttribute($request->field);
+
+        $field = null;
+        foreach ($fields as $f) {
+            $fieldAttribute = $f->attribute;
+
+            // Handle normal Image/File fields
+            if(isset($fieldAttribute) && $fieldAttribute == $request->field) {
+                $field = $f;
+                break;
+            }
+
+            // Handle Image/File fields inside Flexible
+            if($f instanceof Flexible) {
+                [$key, $attribute] = explode('__', $request->field);
+                foreach ($f->value as $index => $value) {
+                    if($key == $value['key']) {
+                        foreach ($value['attributes'] as $attributeIndex => $attributeValue) {
+                            if($attributeValue['attribute'] == $attribute) {
+                                $field = File::make('Image', "$fieldAttribute->$attributeIndex.attributes.$attribute");
+                                $field->disk('gcs');
+                                $field->value = $attributeValue['value'];
+                                break;
+                            }
+                        }
+                    }
+                };
+            }
+        };
 
         if (!($field instanceof File)) {
             abort(404);
